4f2bd7423ea359c7535c136d2353b509e7fc61b0
abdelrahman-ibrahem <abdo.ibrahem1122@gmail.com>
2023-12-07 17:07:13 +0200

    Update installing scripts

--- install_dart.sh
+++ install_dart.sh
@@ -1,5 +1,20 @@
-sudo apt update & sudo apt upgrade -y 
-sudo wget -qO /etc/apt/trusted.gpg.d/dart_linux_signing_key.asc https://dl-ssl.google.com/linux/linux_signing_key.pub
-sudo wget -qO /etc/apt/sources.list.d/dart_stable.list https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list
+#!/bin/bash
+
+# Add Dart repository and key
+sudo apt update
+sudo apt install -y apt-transport-https
+
+# Remove existing Dart GPG key if it exists
+sudo rm /usr/share/keyrings/dart.gpg
+
+# Add Dart GPG key
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /usr/share/keyrings/dart.gpg'
+
+# Add Dart repository to sources.list.d
+echo "deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main" | sudo tee /etc/apt/sources.list.d/dart.list > /dev/null
+
+# Install Dart
 sudo apt update
 sudo apt install -y dart
+
+
